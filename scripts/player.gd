7f30d1bad7e42633505c328c3ee644579ee4cae5
saruulgerel <saruulgerel.o325@gmail.com>
2025-11-28 14:35:50 +0800

    Added basic level geometry (walls & floor)

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,26 +1,54 @@
 extends CharacterBody3D
 
-
-const SPEED = 5.0
+var ORIGINAL_SPEED
+var SPEED = 5.0
+var sprint_drain_amount = 0.3
+var sprint_refresh_amount = 0.4
+var SPRINT_SPEED = 7.0
 const JUMP_VELOCITY = 4.5
+var sprint_slider
 
 
-func _physics_process(delta: float) -> void:
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+func _ready():
+	ORIGINAL_SPEED = SPEED
+	sprint_slider = get_node("/root/" + get_tree().current_scene.name + "/UI/sprint_slider")
+
+func _process(delta):
+	if SPEED == SPRINT_SPEED:
+		sprint_slider.value = sprint_slider.value - sprint_drain_amount * delta
+		if sprint_slider.value == sprint_slider.min_value:
+			SPEED = ORIGINAL_SPEED
+		if SPEED != SPRINT_SPEED:
+			if sprint_slider.value < sprint_slider.max_value:
+				sprint_slider.value = sprint_slider.value + sprint_refresh_amount * delta
+			if sprint_slider.value == sprint_slider.max_value:
+				sprint_slider.visible = false
+				
+func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y -= gravity * delta
 
-	# Handle jump.
+	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var input_dir = Input.get_vector("left", "right", "up", "down")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
+		
+		if Input.is_action_just_pressed("sprint"):
+			sprint_slider.visible = true
+			SPEED = SPRINT_SPEED
+		if Input.is_action_just_released("sprint"):
+			SPEED = ORIGINAL_SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
